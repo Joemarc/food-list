4f7b73472d57b94ff1f59e85f81711259da8e4a0
joe marc <joe.marc3@gmail.com>
2020-04-26 22:24:00 +0200

    push to prod

---
+++ app/javascript/packs/FoodList/config/configuration.js
@@ -0,0 +1,7 @@
+const configuration = {
+  apiUrl: process.env.REACT_APP_API_URL,
+  environment: process.env.REACT_APP_ENVIRONMENT,
+  isProduction: process.env.REACT_APP_IS_PRODUCTION,
+};
+
+export default configuration;
